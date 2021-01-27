f649707083b1564f9a5c98b5cfec764e0fba38ff
Destiny Boling <bolingdestinyr@gmail.com>
2021-01-27 02:55:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
 array.each_with_index do |index, element|
-  puts "#{index + 1}.#{element}"
+  i = 1
+  puts "#{index + i}.#{element}"
 end
 array
   end
